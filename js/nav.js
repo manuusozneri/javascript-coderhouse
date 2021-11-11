b4ec89e86e866c82c66d24674d1cef14f4b633ce
Manuel Usoz <manuelusozneri@gmail.com>
2021-11-11 19:34:43 -0300

    agrego responsive navbar y modifico los archivos

---
+++ js/nav.js
@@ -0,0 +1,23 @@
+
+const navSlide = () => {
+    var burger = document.querySelector('.burger');
+    var nav = document.querySelector('.nav-links');
+    var navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        navLinks.forEach((link, index) => {
+            if (link.style.animation){
+                link.style.animation = '';
+            }else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`;
+            }
+    });
+        burger.classList.toggle('toggle')
+    });
+    
+    
+}
+
+navSlide()
+
